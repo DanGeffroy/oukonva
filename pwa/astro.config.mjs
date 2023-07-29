15c1e2a50b7f8a120f1168e11d2bb140adf99028
DanGeffroy <ogzmovie@gmail.com>
2023-07-30 00:19:56 +0200

    fix: minor issues

--- pwa/astro.config.mjs
+++ pwa/astro.config.mjs
@@ -8,7 +8,7 @@ import svelte from "@astrojs/svelte";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://dangeffroy.github.io/',
-  base: '/oukonvva',
+  base: '/oukonva',
   integrations: [tailwind(), sitemap(), svelte()],
   vite: {
     plugins: [VitePWA({
